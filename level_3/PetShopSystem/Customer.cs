87559b1e833d1bbc88c64aabf417fdf63b9f215e
Garth Groshans II, PhD <garthgroshansii@gmail.com>
2023-08-08 13:07:16 -0400

    add Customer class

---
+++ level_3/PetShopSystem/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Customer
+    {
+        public string Name { get; set; } = "John Doe";
+        public double Balance { get; set; }
+        public List<Pet> Pets { get; set; } = new List<Pet>();
+
+        public Customer() { }
+        public Customer(string name, double balance)
+        {
+            this.Name = name;
+            this.Balance = balance;
+        }
+
+        public bool Adopt(Pet pet)
+        {
+            if (pet.Price < Balance)
+            {
+                Pets.Add(pet);
+                this.Balance -= pet.Price;
+                return true;
+            }
+            return false;
+        }
+
+        public string GetDetails()
+        {
+            var Sb = new StringBuilder();
+            Sb.AppendLine($"Name: {Name}");
+            Sb.AppendLine($"Balance: {Balance}");
+            Sb.AppendLine("Adopted Pets: ");
+
+            foreach (var pet in Pets)
+            {
+                Sb.AppendLine($"{pet.Name} ({pet.Type}) - {pet.Price} ");
+            }
+            return Sb.ToString();
+        }
+
+    }
+}
