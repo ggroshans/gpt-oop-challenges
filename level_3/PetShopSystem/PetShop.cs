bf4bfd1586ea4ad0ebf078c9252686282675c44b
Garth Groshans II, PhD <garthgroshansii@gmail.com>
2023-08-08 13:08:07 -0400

    add PetShop class

---
+++ level_3/PetShopSystem/PetShop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class PetShop
+    {
+        public List<Pet> Pets { get; set; } = new List<Pet>();
+
+        public PetShop() { }
+
+        public void AddPet(Pet pet)
+        {
+            Pets.Add(pet);
+        }
+
+        public void AdoptPet(Customer customer, Pet pet)
+        {
+            if (Pets.Contains(pet) && customer.Balance >= pet.Price)
+            {
+                Pets.Remove(pet);
+                customer.Pets.Add(pet);
+                customer.Balance -= pet.Price;
+            }
+            else
+            {
+                Console.WriteLine("Not enough balance to adopt this pet!");
+            }
+        }
+
+        public string ListPets()
+        {
+            if (Pets.Count == 0) return "No  pets available in the shop.";
+            var SB = new StringBuilder();
+
+            foreach (var pet in Pets)
+            {
+                SB.AppendLine($"{pet.Name} ({pet.Type}) - ${pet.Price}");
+            }
+            return SB.ToString();
+
+        }
+    }
+}
