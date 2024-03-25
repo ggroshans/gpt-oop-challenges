32c7e8531acdfe5f61db794c4e6f012f6c7d9e5d
Garth Groshans II, PhD <garthgroshansii@gmail.com>
2024-03-25 16:19:49 -0400

    create initial weather service implementation

---
+++ level_3/WeatherForecastingApp/WeatherForecastingClassLibrary/Services/WeatherServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherForecastingClassLibrary.Interfaces;
+
+namespace WeatherForecastingClassLibrary.Services
+{
+    public class WeatherServiceProvider : IWeatherService 
+    { 
+        public string GetWeatherData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
